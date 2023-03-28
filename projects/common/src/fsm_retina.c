91bb85828e61497b92212af5979efb40f6b80041
Nerio-Messino <124915706+Nerio-Messino@users.noreply.github.com>
2023-03-29 01:45:58 +0200

    Update fsm_retina.c

--- projects/common/src/fsm_retina.c
+++ projects/common/src/fsm_retina.c
@@ -8,6 +8,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "fsm_retina.h"
+#include "commands.h"
+#include "fsm_button.h"
+#include"fsm_tx.h"
+
 
 /* Defines and enums ----------------------------------------------------------*/
 /* Defines */
@@ -26,8 +30,13 @@ enum
 typedef struct
 {
     fsm_t f; /*!< Retina FSM  */
-    
-    /* TO-DO alumnos */
+    fsm_t *p_fsm_button;
+    uint32_t long_button_press_ms;
+    fsm_t *p_fsm_tx;
+    uint32_t tx_codes_arr[COMMANDS_MEMORY_SIZE];
+    uint8_t tx_codes_index;
+
+
 
 } fsm_retina_t;
 
@@ -46,4 +55,31 @@ fsm_t *fsm_retina_new(fsm_t *p_fsm_button, uint32_t button_press_time, fsm_t *p_
 void fsm_retina_init(fsm_t *p_this, fsm_t *p_fsm_button, uint32_t button_press_time, fsm_t *p_fsm_tx)
 {
     /* TO-DO alumnos */
+    fsm_t *p_fsm = (fsm_t *)(p_this);
+    fsm_init(p_this, fsm_trans_retina);
+    long_button_press_ms();
+    tx_codes_index();
+    tx_codes_arr();
+}
+
+static void do_send_next_msg (fsm_t *p_this){
+ fsm_tx_set_code(p_this, 0x00);
+ fsm_button_reset_duration(p_this);
+ tx_codes_index ++;
+
+}
+
+static bool check_short_pressed (fsm_t *p_this){
+ fsm_t *p_fsm = (fsm_t *)(p_this); 
+ int duration = fsm_button_get_duration(p_fsm);
+ if (duration != 0 && duration <long_button_press_ms()){
+    return true;
+ }else{
+    return false;
+ }
+}
+
+fsm_trans_t fsm_trans_retina[]={
+
+};
 }
